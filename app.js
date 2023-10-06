0c5c9ec16c8806f56a4b4545a7bbbf1dccea4d37
Hyunseo Park <123967536+hyunwestpark@users.noreply.github.com>
2023-10-06 14:46:40 +0900

    Update app.js
    for security, erased data server

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb+srv://admin-hyunseo:henry0828@cluster0.nxgj0it.mongodb.net/todolistDB");
+mongoose.connect("");
 
 const itemSchema = {
   name: String,
